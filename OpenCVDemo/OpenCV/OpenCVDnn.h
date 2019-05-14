d558bcd94953d15afdfefd7b4fb309f6113c293b
Di <849131492@qq.com>
2019-05-14 15:36:56 +0800

    [NF]准备增加新的相关图片处理功能

---
+++ OpenCVDemo/OpenCV/OpenCVDnn.h
@@ -0,0 +1,17 @@
+//
+//  OpenCVDnn.h
+//  OpenCVDemo
+//
+//  Created by Di on 2019/5/14.
+//  Copyright © 2019 chouheiwa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OpenCVDnn : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OpenCVDemo/OpenCV/OpenCVDnn.mm
@@ -0,0 +1,13 @@
+//
+//  OpenCVDnn.m
+//  OpenCVDemo
+//
+//  Created by Di on 2019/5/14.
+//  Copyright © 2019 chouheiwa. All rights reserved.
+//
+
+#import "OpenCVDnn.h"
+#import <opencv2/dnn.hpp>
+@implementation OpenCVDnn
+
+@end
